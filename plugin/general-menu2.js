fbaeca3043702d23621095db85763bb1b29d603a
afgan1554 <108453129+afgan1554@users.noreply.github.com>
2022-07-20 09:02:49 +0700

    Update and rename plugins/general-menu2.js to plugin/general-menu2.js

---
+++ plugin/general-menu2.js
@@ -0,0 +1,14 @@
+import moment from 'moment-timezone'
+let handler = async (m, { conn, usedPrefix, __dirname, text, command }) => {
+let date = moment.tz('Asia/Jakarta').format("dddd, Do MMMM, YYYY")
+let time = moment.tz('Asia/Jakarta').format('HH:mm:ss')
+conn.sendHydrated(m.chat, '*『 ɪɴᴛʀᴏᴅᴜᴄᴛɪᴏɴ 』*', `Hai, silahkan klik tombol menu untuk menampilkan menu bot\n\n-----------[ INFO BOT ]----------&&\nName bot : kanna BOT\nNama Owner : Afgan  ID\nNomor Owner : wa.me/6282125968810\n\nᴅᴀᴛᴇ: ${date}\nᴛɪᴍᴇ: ${time} ﹙ɢᴍᴛ +7:00)`.trim(), './media/broadcast.jpg', 'https://wa.me/6282125968810', 'Chat Owner 🔖', null, null, [
+[`ᴍᴇɴᴜ`, '.m'],
+[`\nɢᴡᴇʜ ꜱᴀᴍɴɢᴇ ᴅᴇɴɢᴀɴ ᴀɴɪᴍᴇʜ`, 'Bilek']
+], m, {asLocation: true})
+}
+handler.help = ['menu']
+handler.tags = ['general']
+handler.command = /^(menu)$/i
+
+export default handler
